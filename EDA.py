5a2bfb4fb7a77b210d9596e7ee900e81604dddb8
Claire Bassett <clairebassett@Claires-MacBook-Air.local>
2025-11-29 11:57:01 -0500

    Adding EDA

---
+++ EDA.py
@@ -0,0 +1,12 @@
+# Cleaning Script
+
+# English check
+
+# Check for rows where operation != create and type != type
+
+# Delete unneeded rows - operation and type, timestamp?
+
+# Delete rows with no text value
+
+
+
